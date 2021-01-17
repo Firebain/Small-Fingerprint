cd64f6845a688b483b0401c16d334522f0d4e517
Maxim <maxim230898@yandex.ru>
2021-01-17 14:05:50 +0400

    show info

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
-alert(123);
+const addToPage = (text) => {
+  const p = document.createElement("p");
+  p.appendChild(document.createTextNode(text));
+  document.body.appendChild(p);
+};
+
+addToPage(
+  `Размер экрана: ${
+    (0 | (window.screen.width * (window.devicePixelRatio || 1))) +
+    "x" +
+    (0 | (window.screen.height * (window.devicePixelRatio || 1))) +
+    "x" +
+    (0 | window.screen.colorDepth)
+  }`
+);
+
+addToPage(
+  `Размер окна: ${
+    (0 |
+      (window.innerWidth ||
+        document.documentElement.clientWidth ||
+        document.body.clientWidth)) +
+    "x" +
+    (0 |
+      (window.innerHeight ||
+        document.documentElement.clientHeight ||
+        document.body.clientHeight))
+  }`
+);
+
+addToPage(`User Agent: ${window.navigator.userAgent}`);
